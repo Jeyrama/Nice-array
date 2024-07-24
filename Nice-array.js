acc84054e1f8ebd8208cbd4dca34da37bb994b37
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-07-23 19:55:59 -0700

    Update Nice-array.js

--- Nice-array.js
+++ Nice-array.js
@@ -24,3 +24,12 @@ An empty array is not considered nice.
 
 
 // Solution
+
+function isNice(arr){
+  let counter = 0
+  for(let i = 0; i < arr.length; i++) {
+      if (arr.includes(arr[i] - 1) || arr.includes(arr[i] + 1)) counter ++
+  }
+  if (counter == arr.length && arr.length != 0) return true
+  return false
+}
